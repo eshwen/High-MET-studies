d188387842567e045501d8aef09cdfa0c1fd5cc7
Eshwen <eshwen.bhal@bristol.ac.uk>
2019-06-17 15:46:43 +0200

    Adding architecture check

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
+if [[ "$SCRAM_ARCH" == "slc7_"* ]]; then
+    export SCRAM_ARCH=slc7_amd64_gcc630
+else
+    export SCRAM_ARCH=slc6_amd64_gcc630
+fi
 cmsrel CMSSW_9_2_8  # works on both SLC6 and CentOS7 machines
 pushd CMSSW_9_2_8/src
 cmsenv
